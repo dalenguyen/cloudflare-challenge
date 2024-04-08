080bbc868f6bf75957f779ecf6b5c8b8cf2f2ddf
Dale Nguyen <dungnq@itbox4vn.com>
2024-04-07 23:38:58 -0400

    test reactive form

--- ai/src/app/pages/(home).page.ts
+++ ai/src/app/pages/(home).page.ts
@@ -9,7 +9,13 @@ import { AiService } from "../services/ai.service";
 import { firstValueFrom } from "rxjs";
 import { CommonModule } from "@angular/common";
 import { AudioRecorderComponent } from "../components/audio.component";
-import { FormsModule } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 @Component({
   selector: "ai-home",
   standalone: true,
@@ -18,22 +24,23 @@ import { FormsModule } from "@angular/forms";
 
     <app-audio-recorder (audioReady)="getTextFromAudio($event)" />
 
-    <input
-      pInput
-      type="text"
-      id="text"
-      [ngModel]="prompt"
-      (ngModelChange)="prompt = $event"
-      class="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="cyperpunk cat"
-      required
-    />
-    <button
-      class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-      (click)="onSubmit()"
-    >
-      Generate Image
-    </button>
+    <form [formGroup]="form">
+      <input
+        pInput
+        type="text"
+        id="text"
+        formControlName="prompt"
+        class="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="cyperpunk cat"
+        required
+      />
+      <button
+        class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        (click)="onSubmit()"
+      >
+        Generate Image
+      </button>
+    </form>
 
     @if (response?.result) {
       <div>
@@ -47,21 +54,30 @@ import { FormsModule } from "@angular/forms";
       <p class="text-red-500">{{ errorMessage }}</p>
     }
   `,
-  imports: [CommonModule, AudioRecorderComponent, FormsModule],
+  imports: [
+    CommonModule,
+    AudioRecorderComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export default class HomeComponent {
   private aiService = inject(AiService);
   private cd = inject(ChangeDetectorRef);
 
-  prompt = "test";
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    prompt: [""],
+  });
 
   response: { result: string } | undefined;
   errorMessage = "";
 
   async onSubmit() {
-    console.log(`prompt`, this.prompt);
+    console.log(`prompt`, this.form.value.prompt);
 
-    if (this.prompt.length < 10) {
+    if (this.form.value.prompt!.length < 10) {
       this.errorMessage = "Prompt length must greater than 10 characters!";
       return;
     }
@@ -70,7 +86,7 @@ export default class HomeComponent {
 
     try {
       this.response = await firstValueFrom(
-        this.aiService.getImageFromText(this.prompt),
+        this.aiService.getImageFromText(this.form.value.prompt!),
       );
     } catch (error: any) {
       this.errorMessage = error?.message;
@@ -86,7 +102,9 @@ export default class HomeComponent {
 
       console.log(response);
 
-      this.prompt = response.text;
+      this.form.patchValue({
+        prompt: response.text,
+      });
       this.cd.detectChanges();
     } catch (error: any) {
       this.errorMessage = error?.message;
