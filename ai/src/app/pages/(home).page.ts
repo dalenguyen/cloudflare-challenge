7ae5ec58eabe6c9c1c64416f0b5aa8bbdb5f5429
Dale Nguyen <dale.nguyen@noibu.com>
2024-04-24 21:13:41 -0400

    push test

--- ai/src/app/pages/(home).page.ts
+++ ai/src/app/pages/(home).page.ts
@@ -21,7 +21,6 @@ import { FormsModule } from "@angular/forms";
     <app-audio-recorder (audioReady)="getTextFromAudio($event)" />
 
     <input
-      #input
       type="text"
       [(ngModel)]="prompt"
       id="text"
@@ -29,6 +28,8 @@ import { FormsModule } from "@angular/forms";
       placeholder="cyperpunk cat"
       required
     />
+
+    <pre>{{ prompt() }}</pre>
 
     <button
       class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
