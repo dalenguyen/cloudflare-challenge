bcd6a541f5c2da0435acd9d5c5fc33e0ba29fd93
Dale Nguyen <dale.nguyen@noibu.com>
2024-04-24 21:20:46 -0400

    remove log

--- ai/src/app/pages/(home).page.ts
+++ ai/src/app/pages/(home).page.ts
@@ -29,8 +29,6 @@ import { FormsModule } from "@angular/forms";
       required
     />
 
-    <pre>{{ prompt() }}</pre>
-
     <button
       class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
       (click)="onSubmit()"
@@ -64,14 +62,6 @@ export default class HomeComponent {
   imageDescription = signal("");
   errorMessage = signal("");
   loading = signal(false);
-
-  constructor() {
-    effect(() => {
-      if (this.prompt()) {
-        console.log(`Current prompt: `, this.prompt());
-      }
-    });
-  }
 
   async onSubmit() {
     this.errorMessage.set("");
